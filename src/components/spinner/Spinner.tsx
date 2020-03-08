eafbc2bdb5805f9f5ac84a8bfe6fcb1e0a6902c3
Sander F. Sandøy <sanderfsandoy@hotmail.com>
2020-03-08 14:58:56 +0100

    - Add Spinner-component

--- src/components/spinner/Spinner.tsx
+++ src/components/spinner/Spinner.tsx
@@ -8,7 +8,6 @@ interface Props {
 }
 
 const Spinner: React.FunctionComponent<Props> = ({ size }: Props) => {
-  // TODO APPLY WIDTH somehow
   return <div className={`spinner spinner-${size}`} />;
 };
 
